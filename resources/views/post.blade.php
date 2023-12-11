56a7355f8bd3f602f3db34d992ca7c5069822744
Chris Williamson <chriswill0629@gmail.com>
2023-12-11 16:42:45 -0500

    Learning laravel

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,10 +5,13 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My blog</title>
-    <link rel="stylesheet" href="./css/app.css">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/app.css">
+
 </head>
 <body>
-    <h1>Hello World</h1>
+<?= $post ?>
+
+<a href="/posts">Go Back</a>
 </body>
 </html>
